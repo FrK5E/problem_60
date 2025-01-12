eff3a5947d15c9a89585a496adcf997892935a39
FrK5E <FrK5Ewzb4XbBN1aWlGUZ@proton.me>
2025-01-12 21:31:58 +0100

    trial and error

--- src/main.rs
+++ src/main.rs
@@ -26,11 +26,11 @@ fn concat(a: usize, b: usize) -> usize {
 fn test(trial: &Vec<usize>, primes: &Vec<bool>) -> bool {
     for i in trial {
         for j in trial {
-            if i==j { 
+            if i == j {
                 continue;
             }
             let k = concat(*i, *j);
-            if ! primes[k] { 
+            if !primes[k] {
                 return false;
             }
         }
@@ -40,40 +40,45 @@ fn test(trial: &Vec<usize>, primes: &Vec<bool>) -> bool {
 }
 
 fn explore(trial: &Vec<usize>, primes: &Vec<bool>) {
-    let start = trial[trial.len()-1]+1;
+    let start = trial[trial.len() - 1] + 1;
     let max = primes.len();
+    
     for i in start..max {
         if !primes[i] {
             continue;
         }
         let mut trial2 = trial.clone();
         trial2.push(i);
-        let flag = test( &trial2, &primes);
-        if flag { 
-            println!( "found ");
-            for i in trial2{ 
-                print!( " {} ", i)
+        
+        if concat(trial2[trial2.len()-1], trial2[trial2.len()-2]) >= primes.len() { 
+            break;
+        }
+        let flag = test(&trial2, &primes);
+        if flag {
+            println!("found ");
+            for i in trial2 {
+                print!(" {} ", i)
             }
         }
     }
 }
 
 fn main() {
-
-    let mut data = Vec::with_capacity(10000);
-    data.resize(10000, true);
+    let n = 100000000;
+    let mut data = Vec::with_capacity(n);
+    data.resize(n, true);
 
     init_primes(&mut data);
 
-    for i in 3..data.len()-1{ 
-        if data[i] { 
-            let trial = vec![i];
-            explore( &trial, &data);
+    let trial = vec![3, 7, 109, 673];
+    explore( &trial, &data);
 
-        }
-        
-    }
-
+    // for i in 3..data.len() - 1 {
+    //     if data[i] {
+    //         let trial = vec![i];
+    //         explore(&trial, &data);
+    //     }
+    // }
 }
 
 #[cfg(test)]
@@ -89,40 +94,38 @@ mod tests {
     }
 
     #[test]
-    fn test_primes() { 
+    fn test_primes() {
         let mut data = Vec::with_capacity(800);
         data.resize(800, true);
         init_primes(&mut data);
 
-        assert_eq!( data[3], true);
-        assert_eq!( data[7], true);
-        assert_eq!( data[109], true); 
-        assert_eq!( data[673], true);
-        assert_eq!( data[674], false);
+        assert_eq!(data[3], true);
+        assert_eq!(data[7], true);
+        assert_eq!(data[109], true);
+        assert_eq!(data[673], true);
+        assert_eq!(data[674], false);
     }
 
     #[test]
-    fn test_criterion(){
-        let n: usize  = 673109 + 2; 
+    fn test_criterion() {
+        let n: usize = 673109 + 2;
         let mut data = Vec::with_capacity(n);
         data.resize(n, true);
         init_primes(&mut data);
-        let trial = vec![3,7,109, 673];
-        assert_eq!( test(&trial, &data), true);
+        let trial = vec![3, 7, 109, 673];
+        assert_eq!(test(&trial, &data), true);
 
-        let trial2 = vec![3,7,11,673];
-        assert_eq!( test(&trial2, &data), false );
-
+        let trial2 = vec![3, 7, 11, 673];
+        assert_eq!(test(&trial2, &data), false);
     }
 
     #[test]
-    fn test_criterion2(){
-        let n: usize  = 673109 + 2; 
+    fn test_criterion2() {
+        let n: usize = 673109 + 2;
         let mut data = Vec::with_capacity(n);
         data.resize(n, true);
         init_primes(&mut data);
-        let trial = vec![3,7];
-        assert_eq!( test(&trial, &data), true);
-
+        let trial = vec![3, 7];
+        assert_eq!(test(&trial, &data), true);
     }
 }
